83e0167d0e65ec4d9d27e59002fc47a13b17989c
nsns0101 <nsns0101@naver.com>
2020-02-18 20:15:10 +0900

    fetch_option(limit,minimum_rating)

--- graphql/db.js
+++ graphql/db.js
@@ -2,15 +2,24 @@ import fetch from "node-fetch";
 const API_URL = "https://yts.am/api/v2/list_movies.json";
 
 
-export const getMovies = () => {
-    return fetch(`${API_URL}`)
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `limit${limit}`;
+    }
+
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
         .then(function (res) {
             return res.json();
         })
-        .then((json) => {
+        .then(function (json) {
+            console.log(json);
             return json.data.movies;
         })
         .catch(function (error) {
             console.log(error);
-        });
+        })
 }
