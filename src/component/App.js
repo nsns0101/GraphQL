0941a5e3eaa515d97911d8ec21b5ee7ce7fb5215
nsns0101 <nsns0101@naver.com>
2020-02-24 17:31:20 +0900

    setting_Router and Styles

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { HashRouter, Route, Router } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <HashRouter>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/:id" component={Detail} />
+    </HashRouter>
   );
 }
 
